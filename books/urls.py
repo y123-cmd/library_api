e4dba4c65608cc8c81b95c357e4b16ac20451a2b
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-03-18 10:51:18 +0300

    added an api/token endpoint

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import BookListCreateView, BookDetailView, BorrowBookView, ReturnBookView
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('borrow/', BorrowBookView.as_view(), name='borrow-book'),
     path('return/<int:pk>/', ReturnBookView.as_view(), name='return-book'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
--- library/settings.py
+++ library/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     "books",
     "rest_framework.authtoken",
+    "django_extensions",
 ]
 
 REST_FRAMEWORK = {
